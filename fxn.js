3d28efa680cfab107ea32ad642b069a330b165ff
Pushp Raj <raj.pushp05@gmail.com>
2018-06-23 19:55:20 +0530

    1.0.0

---
+++ fxn.js
@@ -0,0 +1,62 @@
+function valid()
+{
+	if(document.reg.fname.value=="")
+	{
+		alert("Please Enter the First name");
+	}
+	if(document.reg.lname.value=="")
+	{
+		alert("Please Enter the Last name");
+	}
+	if(document.reg.restname.value=="")
+	{
+		alert("Please Enter full name");
+	}
+	if(document.reg.uname.value=="")
+	{
+		alert("Please Enter Username");
+	}
+	if(document.reg.pass.value=="")
+	{
+		alert("Please Enter Password");
+	}
+	if(document.reg.zipcode.value=="")
+	{
+		alert("Please Enter valid zipcode");
+	}
+	if(document.reg.mobile.value=="")
+	{
+		alert("Please Enter valid Mobile Number");
+	}
+	if(document.reg.email.value=="")
+	{
+		alert("Please Enter valid email");
+	}
+	if(doocument.reg.weblink.value=="")
+	{
+		alert("Please Enter valid Website address")
+	}
+	if(document.reg.loc.value=="")
+	{
+		alert("Please Enter valid Address");
+	}
+	if(document.reg.country.value=="null")
+	{
+		alert("Please Select a country");
+	}
+}
+function msg()
+{
+	alert("This site is best viewed in google chrome.");
+}
+function validlog()
+{
+	if(document.log.uname.value=="")
+	{
+		alert("Enter username..");
+	}
+	if(document.log.pass.value=="")
+	{
+		alert("Enter password");
+	}
+}
